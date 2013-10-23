68c991ade8ea9d9abf6194fda310817520bd2729
William Wallberg <wwallberg@gmail.com>
2013-10-22 20:44:58 -0400

    Initialize repository

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+#ruby-gemset=railstutorial_rails_4_0
+
+gem 'rails', '4.0.0'
+
+group :development do
+  gem 'mysql2'
+end
+
+gem 'aws-sdk', '~> 1.0'
+gem 'sass-rails', '4.0.1'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '4.0.1'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.0.2'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BillSite::Application.config.secret_key_base = '4006abecc377b37eff770ead9511ec6ea08808058fc32979a914ae36b4172438778b21dcb758a521d0af9b134868ff08a59106d615a3222528be848c687a6a08'
